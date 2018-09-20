a82e861eae45f21beeff30b4c9d20abe00d04d8d
David Xie <david.xie@complianceexperts.com>
2018-09-20 16:23:16 +1000

    Support multiple files selection
    Support multiple files selection

--- src/windows/WindowsFilePickerProxy.js
+++ src/windows/WindowsFilePickerProxy.js
@@ -1,43 +1,93 @@
 
 module.exports = {
+    pickFile: function (successCallback, errorCallback, args) {
+        var fileTypes = args[0];
+        var fileTypeFilter = ["*"];
 
-	open: function (successCallback, errorCallback, args) {
-		var fileTypes = args[0];
-		var fileTypeFilter = ["*"];
-		
-		if(fileTypes && fileTypes instanceof Array && fileTypes.length > 0)
-			fileTypeFilter = fileTypes;
-			
-		// Create the picker object and set options
-		var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
-		openPicker.viewMode = Windows.Storage.Pickers.PickerViewMode.list;
-		openPicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
-		openPicker.fileTypeFilter.replaceAll(fileTypeFilter);
+        if (fileTypes && fileTypes instanceof Array && fileTypes.length > 0)
+            fileTypeFilter = fileTypes;
 
-		// Open the picker for the user to pick a file
-		openPicker.pickSingleFileAsync().done(function (file) {
-			if (file) {
-				
-				var storageFolder = Windows.Storage.ApplicationData.current.localFolder;
-				file.copyAsync(storageFolder, file.name, Windows.Storage.NameCollisionOption.replaceExisting).done(function (storageFile) {
-					
-					successCallback("ms-appdata:///local/" + storageFile.name);
-					
-				}, function () {
-					errorCallback("Can't access localStorage folder.");
-				});								
-				
-			} else {
-				// The picker was dismissed with no selected file
-				errorCallback("User didn't choose a file.");			
-			}
-			
-		}, function (errror) {
-			errorCallback("User didn't choose a file.");
-	    });
-		
-	}
-		
+        // Create the picker object and set options
+        var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+        openPicker.viewMode = Windows.Storage.Pickers.PickerViewMode.list;
+        openPicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
+        openPicker.fileTypeFilter.replaceAll(fileTypeFilter);
+
+        // Open the picker for the user to pick a file
+        openPicker.pickSingleFileAsync().done(function (file) {
+            if (file) {
+
+                var storageFolder = Windows.Storage.ApplicationData.current.localFolder;
+                file.copyAsync(storageFolder, file.name, Windows.Storage.NameCollisionOption.replaceExisting).done(function (storageFile) {
+
+                    successCallback("ms-appdata:///local/" + storageFile.name);
+
+                }, function () {
+                    errorCallback("Can't access localStorage folder.");
+                });
+
+            } else {
+                // The picker was dismissed with no selected file
+                errorCallback("User didn't choose a file.");
+            }
+
+        }, function (errror) {
+            errorCallback("User didn't choose a file.");
+        });
+    },
+    pickFiles: function (successCallback, errorCallback, args) {
+        var fileTypes = args[0];
+        var fileTypeFilter = ["*"];
+
+        if (fileTypes && fileTypes instanceof Array && fileTypes.length > 0)
+            fileTypeFilter = fileTypes;
+
+        // Create the picker object and set options
+        var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+        openPicker.viewMode = Windows.Storage.Pickers.PickerViewMode.list;
+        openPicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
+        openPicker.fileTypeFilter.replaceAll(fileTypeFilter);
+
+        // Open the picker for the user to pick a file
+        openPicker.pickMultipleFilesAsync().done(function (files) {
+            if (files.size > 0) {
+                var copyFilePromises = [];
+                var fileUris = [];
+
+                for (var i = 0; i < files.size; i++) {
+                    var file = files[i];
+
+                    if (file) {
+                        var storageFolder = Windows.Storage.ApplicationData.current.localFolder;
+
+                        var promise = new WinJS.Promise(function (resolve, reject) {
+                            file.copyAsync(storageFolder, file.name, Windows.Storage.NameCollisionOption.replaceExisting).done(function (storageFile) {
+                                fileUris.push("ms-appdata:///local/" + storageFile.name);
+                                resolve();
+                            }, function () {
+                                errorCallback("Can't access localStorage folder.");
+                                resolve();
+                            });
+                        });
+
+                        copyFilePromises.push(promise);
+                    } else {
+                        // The picker was dismissed with no selected file
+                        errorCallback("Failed to retrieve the file.");
+                    }
+                }
+
+                WinJS.Promise.join(copyFilePromises).done(function () {
+                    successCallback(fileUris);
+                });
+            } else {
+                // The picker was dismissed with no selected file
+                errorCallback("User didn't choose a file.");
+            }
+        }, function (errror) {
+            errorCallback("User didn't choose a file.");
+        });
+    }
 };
 
 require("cordova/exec/proxy").add("WindowsFilePicker", module.exports);
--- www/WindowsFilePicker.js
+++ www/WindowsFilePicker.js
@@ -1,19 +1,20 @@
-(function(window) {
- 
- var WindowsFilePicker = function() {};
-  
-    WindowsFilePicker.prototype = {
-    
-        open: function(success, fail, fileTypes) {
-            cordova.exec(success, fail, "WindowsFilePicker", "open", [fileTypes]);
-        }
+(function (window) {
 
+    var WindowsFilePicker = function () { };
+
+    WindowsFilePicker.prototype = {
+        pickFile: function (success, fail, fileTypes) {
+            cordova.exec(success, fail, "WindowsFilePicker", "pickFile", [fileTypes]);
+        },
+        pickFiles: function (success, fail, fileTypes) {
+            cordova.exec(success, fail, "WindowsFilePicker", "pickFiles", [fileTypes]);
+        }
     };
-	
+
     window.WindowsFilePicker = new WindowsFilePicker();
-    
+
     // backwards compatibility
     window.plugins = window.plugins || {};
     window.plugins.WindowsFilePicker = window.WindowsFilePicker;
-  
+
 })(window);
